e6a7cb354c13daa1f9c88d9f45f770ec442be73d
마경욱 <kyeongwook.ma@liveo.me>
2020-03-01 20:16:34 +0900

    1. implement parse args logic 2. create inital UI with QTableWidget

--- Board.py
+++ Board.py
@@ -1,28 +1,26 @@
+from PyQt5.QtGui import QColor
+from PyQt5.QtWidgets import QTableWidgetItem
 from PyQt5.QtWidgets import QWidget, QTableWidget, QVBoxLayout
 
-
 class GoL(QWidget):
+
     def __init__(self, row, col):
         super().__init__()
-        self.title = 'PyQt5 table - pythonspot.com'
+        self.title = 'PyQt5 table'
         self.left = 0
         self.top = 0
-        self.width = 300
-        self.height = 200
+        self.width = 2000
+        self.height = 2000
 
-        self.initUI(row, col)
-
-    def initUI(self, row, col):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-
         self.createTable(row=row, col=col)
 
         self.layout = QVBoxLayout()
         self.layout.addWidget(self.tableWidget)
         self.setLayout(self.layout)
 
-        self.show()
+        self.showFullScreen()
 
     def createTable(self, row, col):
         self.tableWidget = QTableWidget()
@@ -30,3 +28,17 @@ class GoL(QWidget):
         self.tableWidget.setColumnCount(col)
         self.tableWidget.resizeColumnsToContents()
         self.tableWidget.move(0, 0)
+
+        for r in range(row):
+            for c in range(col):
+                self.tableWidget.setItem(r, c, QTableWidgetItem())
+                self.tableWidget.item(r, c).setBackground(QColor(0, 0, 0))
+
+    def update_board(self):
+        pass
+
+    def set_status_on(self, row, col):
+        pass
+
+    def set_status_off(self, row, col):
+        pass
---
+++ game_of_life.py
@@ -0,0 +1,52 @@
+import Board
+import sys
+from random import randint
+
+from PyQt5.QtTest import QTest
+from PyQt5.QtWidgets import QApplication
+
+
+def main():
+    args = sys.argv
+
+    len_args = len(args)
+    num_rows, num_cols = 0, 0
+    cells_loc = []
+
+    if len_args == 1:
+        num_rows = randint(40, 80)
+        num_cols = randint(80, 120)
+        cells_loc = [(randint(40, num_rows) - 1, randint(80, num_cols - 1)) for n in range(2000)]
+
+    elif len_args == 2:
+        num_rows, num_cols, nums_cell_init = parse_args(args)
+        cells_loc = parse_cells_loc(args)
+
+    elif len_args == 3:
+        num_rows, num_cols, nums_cell_init = parse_args(args)
+        cells_loc = parse_cells_loc(args)
+
+    app = QApplication(sys.argv)
+    ui = Board.GoL(num_rows, num_cols)
+
+    for c in cells_loc:
+        ui.set_status_on(c[0], c[1])
+
+    while True:
+        QTest.qWait(100)
+        ui.update_board()
+
+    sys.exit(app.exec_())
+
+
+def parse_cells_loc(args):
+    return args[2:]
+
+
+def parse_args(args):
+    num_rows, num_cols = args[0].split(' ')
+    nums_cell_init = args[1]
+    return num_rows, num_cols, nums_cell_init
+
+
+main()
